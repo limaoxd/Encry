9c7b7d85568b48ebe4b200ea8df2c9a247109be3
root <root@MSI.localdomain>
2021-10-23 23:46:03 +0800

    Final Version of BB84.py

--- week_5_bb84/bb84.py
+++ week_5_bb84/bb84.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random as rd
 import sys, os
-#mport matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import qiskit.result.result as result
 
 from qiskit import QuantumCircuit as qc
@@ -15,37 +15,32 @@ provider = IBMQ.load_account()
 class Alice:
     def prepare(self, n):
         qubits = int(4*n,)
-        a = np.random.randint(2, size=qubits,) #Generate a random data bits
-        b = np.random.randint(2, size=qubits,) #Generate a random data bits
-        #print("Alice has generated random secret data bits, a: ", a)
-        #print("She will arbitrarily encode these bits using random basis, b: ", b)
+        a = np.random.randint(2, size=qubits,)  #Generate a random data bits
+        b = np.random.randint(2, size=qubits,)  #Generate a random data bits
         return (a, b)
 
 class Bob:
     def genRandomBasis(self, n):
         qubits = int(4*n)
-        b = np.random.randint(2, size=qubits,)
-        #print("Bob has chosen random basis, b': ", b)
+        b = np.random.randint(2, size=qubits,)  #Generate a random data bits
         return b
 
 class Eve:
     def genRandomBasis(self, n):
         qubits = int(4*n)
-        b = np.random.randint(2, size=qubits,)
-        #print("Eve has chosen random basis: ", b)
+        b = np.random.randint(2, size=qubits,)  #Generate a random data bits
         return b
 
 def BB84(keylen):
     qubits = int(4*keylen)
-
     n = keylen
 
-    qr_1 = qr(name="qr_1", size=qubits)
-    qr_2 = qr(name="qr_2", size=qubits)
-    cr_1 = cr(name="cr_1", size=qubits)
-    cr_2 = cr(name="cr_2", size=qubits)
-    bbCir_1 = qc(qr_1, cr_1, name="BB84")
-    bbCir_2 = qc(qr_2, cr_2, name="BB84_Eve")
+    qr_1 = qr(name="qr_1", size=qubits)         #Create a quantum register for Alice, Bob
+    qr_2 = qr(name="qr_2", size=qubits)         #Create a quantum register for Eve
+    cr_1 = cr(name="cr_1", size=qubits)         #Create a classical register for Alice, Bob
+    cr_2 = cr(name="cr_2", size=qubits)         #Create a classical register for Eve
+    bbCir_1 = qc(qr_1, cr_1, name="BB84")       #Connect the Circuit for Alice, Bob
+    bbCir_2 = qc(qr_2, cr_2, name="BB84_Eve")   #Connect the circuit for Eve
 
     #Initialize
     alice = Alice()
@@ -56,28 +51,28 @@ def BB84(keylen):
 
     for i in range(qubits):
         if a[i] == 1:
-            bbCir_1.x(qr_1[i])
-            bbCir_2.x(qr_2[i])
+            bbCir_1.x(qr_1[i])                 
+            bbCir_2.x(qr_2[i])                 
 
     for i in range(qubits):
         if b[i] == 1:
-            bbCir_1.h(qr_1[i])
-            bbCir_2.h(qr_2[i])
+            bbCir_1.h(qr_1[i])                  
+            bbCir_2.h(qr_2[i])                  
 
-    b_bob = bob.genRandomBasis(n)
-    b_eve = eve.genRandomBasis(n)
+    b_bob = bob.genRandomBasis(n)               
+    b_eve = eve.genRandomBasis(n)               
 
     for i in range(qubits):
         if b_eve[i] == 1:
-            bbCir_2.h(qr_2[i])
+            bbCir_2.h(qr_2[i])                  
 
     for i in range(qubits):
         if b_bob[i] == 1:
-            bbCir_1.h(qr_1[i])
-            bbCir_2.h(qr_2[i])
+            bbCir_1.h(qr_1[i])                  
+            bbCir_2.h(qr_2[i])                  
 
     for i in range(qubits):
-        bbCir_1.measure(qr_1[i], cr_1[i])
+        bbCir_1.measure(qr_1[i], cr_1[i])       #Measure 
         bbCir_2.measure(qr_2[i], cr_2[i])
 
     return(bbCir_1, bbCir_2, a, b, b_bob)
@@ -86,7 +81,6 @@ def detKey(a, bob_measure, b, b_bob, n):
     x = [b_i == b_bob_i for (b_i, b_bob_i) in zip(b, b_bob)]
 
     if np.sum(x) < n/2:
-        #print("Aborting protocol. Less than 2n bits match between b and b'")
         return False
     else:
         alice_bits = a[x]
@@ -97,18 +91,13 @@ def detKey(a, bob_measure, b, b_bob, n):
         key_alice = alice_bits[-n:]
         key_bob = bob_bits[-n:]
 
-        #print("Alice has check bits: ", check_alice)
-        #print("Bob has check bits: ", check_bob)
-
         mismatch = [k_a != k_b for (k_a, k_b) in zip(key_alice, key_bob)]
         error = (np.sum(mismatch)) / n
 
         epsilon = 0.05
         if error > epsilon:
-            #print("Interference detected... Aborting!")
             return False
         else:
-            #print("Successfully exchanged private key: ", key_alice)
             return True
 
 def testCir(shots, keylen):
@@ -126,39 +115,27 @@ def testCir(shots, keylen):
 
     return (answer_bob, answer_eve, a, b, b_bob)
 
-keylen = 4
-(answer_bob, answer_eve, a, b, b_bob) = testCir(2**10, keylen)
-#print("Discrete distribution of potential measurements by Bob: ", answer_bob)
-#print("Discrete distribution of potential measurements by Bob with eavesdropping: ", answer_eve)
-
 def getMeasurement(answer, answer_eve):
     bob_measure = list(rd.choice(list(answer.keys())))
     bob_measure = list(map(int, bob_measure))
     bob_measure = np.array(bob_measure)
     bob_measure = bob_measure[::-1]
-    #print("Bob's measurement result: ", bob_measure)
     
     bob_measure_eve = list(rd.choice(list(answer_eve.keys())))
     bob_measure_eve = list(map(int, bob_measure_eve))
     bob_measure_eve = np.array(bob_measure_eve)
     bob_measure_eve = bob_measure_eve[::-1]
-    #print("Bob's measurement result with eavesdropping: ", bob_measure_eve)
     
-    return (bob_measure, bob_measure_eve)
-    
-bob_measure, bob_measure_eve = getMeasurement(answer_bob, answer_eve)
+    return (bob_measure, bob_measure_eve)      
 
-#print("Key Exchange without eavesdropping: ")
-result = detKey(a, bob_measure, b, b_bob, keylen)
-#print("Key Exchange with eavesdropping: ")
-result = detKey(a, bob_measure_eve, b, b_bob, keylen)        
+keylen = 2
 
 for n in [2, 4, 8]:
     succ = 0
     succ_eve = 0
-    for i in range(10):
-        #print(i)
-        # Simulate one measurement with key length n = 4
+    int_range = 20
+
+    for i in range(int_range):
         (answer, answer_eve, a, b, b_bob) = testCir(1, n)
         bob_measure, bob_measure_eve = getMeasurement(answer, answer_eve)
         res = detKey(a, bob_measure, b, b_bob, keylen)
@@ -167,10 +144,8 @@ for n in [2, 4, 8]:
             succ += 1
         res = detKey(a, bob_measure_eve, b, b_bob, keylen)
         # successful
-        if res: 
+        if res:                                     
             succ_eve += 1
         
-    #print("Successful key exchanges without Eve present: ", succ)
-    #print("Successful key exchanges with Eve present: ", succ_eve)
-    p = (succ_eve) / (2**7)
-    print(n," ",p,"\n")
+    p = (succ_eve) / (int_range)
+    print(f"Key Length: {n}, Total Round: {int_range}, Percent of detecting eavesdropping: {p},\n")
